2e708ae17c026d10866d2a2473f2bcb903c0aacd
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2022-08-31 10:54:40 +0300

    first commit

---
+++ src/main/kotlin/ru/spbstu/icfpc2022/main.kt
@@ -0,0 +1,5 @@
+package ru.spbstu.icfpc2022
+
+fun main() {
+    println("Xyu")
+}
---
+++ src/test/kotlin/ru/spbstu/icfpc2022/StupidTest.kt
@@ -0,0 +1,11 @@
+package ru.spbstu.icfpc2022
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFails
+
+class StupidTest {
+    @Test
+    fun `test that everything is fine and tests are running and stuff and you know`() {
+        assertFails { TODO() }
+    }
+}
