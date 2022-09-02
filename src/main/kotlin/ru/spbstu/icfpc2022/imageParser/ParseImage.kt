67117285540d960b074a9b4f4ef9a3640f3f99e4
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2022-09-02 15:55:29 +0300

    parsing images

--- src/main/kotlin/ru/spbstu/icfpc2022/imageParser/ParseImage.kt
+++ src/main/kotlin/ru/spbstu/icfpc2022/imageParser/ParseImage.kt
@@ -1,6 +1,21 @@
 package ru.spbstu.icfpc2022.imageParser
 
 import com.sksamuel.scrimage.ImmutableImage
+import com.sksamuel.scrimage.pixels.Pixel
 import java.io.File
+import java.net.URL
 
-fun parseImage(path: String) = ImmutableImage.loader().fromFile(File(path)).toImmutableImage()
+fun parseImage(path: String) = ImmutableImage.loader().fromFile(File(path))
+fun parseImage(url: URL) = ImmutableImage.loader().fromStream(url.openStream())
+
+fun ImmutableImage.get(x: Int, y: Int) = this.pixel(x, y)
+
+fun main() {
+    val im = parseImage(URL("https://cdn.robovinci.xyz/imageframes/4.png"))
+    var r = im.iterator().asSequence().map { it.red() }.average().toInt()
+    var g = im.iterator().asSequence().map { it.green() }.average().toInt()
+    var b = im.iterator().asSequence().map { it.blue() }.average().toInt()
+    var a = im.iterator().asSequence().map { it.alpha() }.average().toInt()
+
+    println(Pixel(0, 0, r, g, b, a).argb.toULong().toString(16))
+}
