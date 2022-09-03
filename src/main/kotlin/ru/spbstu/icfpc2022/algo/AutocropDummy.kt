6df689c6920dbeb3bb81956c12fd140cb06817fd
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2022-09-03 09:57:45 +0300

    parameterize autocrop

--- src/main/kotlin/ru/spbstu/icfpc2022/algo/AutocropDummy.kt
+++ src/main/kotlin/ru/spbstu/icfpc2022/algo/AutocropDummy.kt
@@ -4,14 +4,16 @@ import ru.spbstu.icfpc2022.algo.tactics.*
 import ru.spbstu.icfpc2022.canvas.*
 import ru.spbstu.icfpc2022.move.*
 
-class AutocropDummy(task: Task) : Solver(task) {
+class AutocropDummy(
+    task: Task,
+    val colorTolerance: Int = 27,
+    val limit: Long = 8000L
+) : Solver(task) {
     override fun solve(): List<Move> {
         var state = PersistentState(
             task,
             Canvas.empty(task.targetImage.width, task.targetImage.height)
         )
-
-        val colorTolerance = 27
 
         val storage = TacticStorage()
         state = AutocropTactic(task, storage, colorTolerance)(state, SimpleId(0))
@@ -20,7 +22,7 @@ class AutocropDummy(task: Task) : Solver(task) {
         val dummyCutter = DummyCutter(
             task,
             storage,
-            3000,
+            limit,
             colorTolerance
         )
         state = dummyCutter.invoke(state)
