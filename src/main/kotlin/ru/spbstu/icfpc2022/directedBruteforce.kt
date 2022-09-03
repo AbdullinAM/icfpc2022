31ecb2606a34eb1ce242c55b8f2381c35270efb2
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2022-09-03 19:48:35 +0300

    directed bruteforce

---
+++ src/main/kotlin/ru/spbstu/icfpc2022/directedBruteforce.kt
@@ -0,0 +1,108 @@
+package ru.spbstu.icfpc2022
+
+import kotlinx.coroutines.*
+import ru.spbstu.icfpc2022.algo.AutocropDummy
+import ru.spbstu.icfpc2022.algo.CuttingTactic
+import ru.spbstu.icfpc2022.algo.RectangleCropDummy
+import ru.spbstu.icfpc2022.algo.Task
+import ru.spbstu.icfpc2022.imageParser.parseImage
+import ru.spbstu.icfpc2022.robovinchi.StateCollector
+import java.net.URL
+import java.util.*
+import java.util.concurrent.ConcurrentLinkedQueue
+
+data class Parameters(
+    val colorTolerance: Int = 25,
+    val pixelTolerance: Int = 15,
+    val limit: Int = 7500,
+    val cuttingTactic: CuttingTactic = CuttingTactic.DUMB
+) {
+    fun neighbours(): Collection<Parameters> = setOf(
+        copy(colorTolerance = colorTolerance + 1),
+        copy(colorTolerance = (colorTolerance - 1).coerceAtLeast(0)),
+        copy(pixelTolerance = pixelTolerance + 1),
+        copy(pixelTolerance = (pixelTolerance - 1).coerceAtLeast(0)),
+        copy(limit = limit + 200),
+        copy(limit = (limit - 200).coerceAtLeast(0)),
+    ) + CuttingTactic.values().filter { it != cuttingTactic }.map { copy(cuttingTactic = it) }
+}
+
+fun main(args: Array<String>) {
+    try {
+        val problems = getProblems()
+        val submissions = submissions()
+        val bestSubmissions = submissions.bestSubmissions()
+
+        val taskSpec = args.first().split("-")
+        val taskIds = when {
+            taskSpec.size == 1 -> listOf(taskSpec.single().toInt())
+            taskSpec.size == 2 -> (taskSpec[0].toInt() .. taskSpec[1].toInt())
+            else -> taskSpec.map { it.toInt() }
+        }
+        runBlocking {
+            withContext(Dispatchers.Default) {
+                forEachAsync(taskIds) { taskId ->
+                    val problem = problems.first { it.id == taskId }
+                    val im = problem.target
+                    val bestScore = bestSubmissions[problem.id]?.score
+                    var task = Task(problem.id, im, problem.initialConfig, bestScore = bestScore)
+                    StateCollector.turnMeOff = true
+
+                    val visited: MutableSet<Parameters> = Collections.synchronizedSet(mutableSetOf<Parameters>())
+                    val que = ConcurrentLinkedQueue<Parameters>()
+                    que.add(Parameters())
+
+                    var currentScore = Long.MAX_VALUE
+                    var currentWinner = Parameters()
+                    while (que.isNotEmpty()) {
+                        val next = que.poll()
+                        visited.add(next)
+
+                        val top3 = mapAsync(next.neighbours().filter { it !in visited }.asIterable()) {
+                            val colorTolerance = it.colorTolerance
+                            val pixelTolerance = it.pixelTolerance
+                            val limit = it.limit
+                            val cutterTactic = it.cuttingTactic
+                            try {
+                                println("Parameters: taskId = $taskId, colorTolerance = $colorTolerance, pixelTolerance = ${pixelTolerance * 0.05}, limit = $limit, cutterTactic = $cutterTactic")
+                                val rectangleCropDummy = RectangleCropDummy(
+                                    task,
+                                    colorTolerance,
+                                    pixelTolerance * 0.05,
+                                    limit.toLong(),
+                                    cutterTactic
+                                )
+                                val solution = rectangleCropDummy.solve()
+                                if (solution.score < task.bestScoreOrMax) {
+                                    println("Succeeded with parameters: taskId = $taskId, colorTolerance = $colorTolerance, pixelTolerance = ${pixelTolerance * 0.05}, limit = $limit, cutterTactic = $cutterTactic")
+                                    submit(problem.id, solution.commands.joinToString("\n"))
+                                    task = Task(problem.id, im, problem.initialConfig, bestScore = solution.score)
+                                } else {
+                                    println("Finished with parameters: taskId = $taskId, colorTolerance = $colorTolerance, pixelTolerance = ${pixelTolerance * 0.05}, limit = $limit, cutterTactic = $cutterTactic")
+                                }
+                                it to solution.score
+                            } catch (e: Throwable) {
+                                System.err.println("Failed with parameters: taskId = $taskId, colorTolerance = $colorTolerance, pixelTolerance = ${pixelTolerance * 0.05}, limit = $limit, cutterTactic = $cutterTactic")
+                                it to Long.MAX_VALUE
+                            }
+                        }.sortedBy { it.second }.take(3)
+
+                        for ((neighbour, score) in top3) {
+                            if (score < currentScore) {
+                                currentScore = score
+                                currentWinner = neighbour
+                                que.add(neighbour)
+                            }
+                        }
+                    }
+
+                    println("Task#$taskId\nlast score: $currentScore\nwith parameters $currentWinner")
+                }
+            }
+        }
+
+
+    } finally {
+        shutdownClient()
+    }
+}
